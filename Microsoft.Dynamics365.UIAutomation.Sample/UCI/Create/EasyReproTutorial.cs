f0dd9d448102dd8b2bac72b1a15146a089952759
BakkappaN <bakkappac@gmail.com>
2023-01-11 14:43:53 +0530

    Screen capture code added

--- Microsoft.Dynamics365.UIAutomation.Sample/UCI/Create/EasyReproTutorial.cs
+++ Microsoft.Dynamics365.UIAutomation.Sample/UCI/Create/EasyReproTutorial.cs
@@ -1,18 +1,22 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Dynamics365.UIAutomation.Api.UCI;
-using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UIAutomation.Sample.Util;
 using System;
 using System.Security;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System.Drawing;
+using OpenQA.Selenium;
+
 
 namespace Microsoft.Dynamics365.UIAutomation.Sample.UCI
 {
     [TestClass]
-    public class EasyReproTutorial
+    public class EasyReproTutorial : TestBase
     {
-        public TestContext TestContext { get; set; }
+        // public TestContext TestContext { get; set; }
 
         private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
         private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
@@ -20,147 +24,125 @@ namespace Microsoft.Dynamics365.UIAutomation.Sample.UCI
         private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
 
         [TestMethod]
+        [TestCategory("TestersTalk")]
         public void EasyReproTest()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
-                xrmApp.CommandBar.ClickCommand("New");
-                //textbox
-                xrmApp.Entity.SetValue("name", TestSettings.GetRandomString(5, 15));
-                xrmApp.Entity.SetValue("telephone1", "1234567897");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.CommandBar.ClickCommand("New");
+            //textbox
+            xrmApp.Entity.SetValue("name", TestSettings.GetRandomString(5, 15));
+            xrmApp.Entity.SetValue("telephone1", "1234567897");
 
-                //Lookup
-                xrmApp.Entity.SetValue("primarycontactid", "alex baker");
-                xrmApp.Lookup.OpenRecord(0);
+            //Lookup
+            xrmApp.Entity.SetValue("primarycontactid", "alex baker");
+            xrmApp.Lookup.OpenRecord(0);
 
-                //TABS
-                xrmApp.Entity.SelectTab("Details");
+            //TABS
+            xrmApp.Entity.SelectTab("Details");
 
-                //Dropdown list
-                xrmApp.Entity.SetValue(new OptionSet { Name = "ownershipcode", Value = "1" });
-                xrmApp.ThinkTime(4000);
-                xrmApp.Entity.Save();
-            }
+            //Dropdown list
+            xrmApp.Entity.SetValue(new OptionSet { Name = "ownershipcode", Value = "1" });
+            xrmApp.ThinkTime(4000);
+            xrmApp.Entity.Save();
         }
-
 
         [TestMethod]
         [TestCategory("SmokeTesting"), TestCategory("TestersTalk")]
         [Priority(0)]
         public void EasyReproTest2()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Contacts");
-                xrmApp.CommandBar.ClickCommand("New");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Contacts");
+            xrmApp.CommandBar.ClickCommand("New");
 
-                //TABS
-                xrmApp.Entity.SelectTab("Details");
+            //TABS
+            xrmApp.Entity.SelectTab("Details");
 
-                var birthDate = new DateTimeControl("birthdate") { Value = DateTime.Now.AddDays(-1000) };
-                xrmApp.Entity.SetValue(birthDate);
-                xrmApp.ThinkTime(4000);
-                xrmApp.Entity.Save();
-            }
+            var birthDate = new DateTimeControl("birthdate") { Value = DateTime.Now.AddDays(-1000) };
+            xrmApp.Entity.SetValue(birthDate);
+            xrmApp.ThinkTime(4000);
+            xrmApp.Entity.Save();
         }
 
         [TestMethod]
-        [TestCategory("SmokeTesting"), TestCategory("TestersTalk")]
+        [TestCategory("TestersTalk")]
         [Priority(0)]
         public void EasyReproTest3()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
-                xrmApp.CommandBar.ClickCommand("New");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.CommandBar.ClickCommand("New");
 
-                var accountName = TestSettings.GetRandomString(5, 15);
-                xrmApp.Entity.SetValue("name", accountName);
-                xrmApp.Entity.Save();
+            var accountName = TestSettings.GetRandomString(5, 15);
+            xrmApp.Entity.SetValue("name", accountName);
+            xrmApp.Entity.Save();
 
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
 
-                xrmApp.Grid.Search(accountName);
+            xrmApp.Grid.Search(accountName);
 
-                xrmApp.ThinkTime(3000);
-            }
+            xrmApp.ThinkTime(3000);
         }
 
         [TestMethod]
         [TestCategory("TestersTalk")]
         public void EasyReproTest4()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
 
-                //Select view
-                // xrmApp.Grid.SwitchView("Active Accounts");
+            //Select view
+            // xrmApp.Grid.SwitchView("Active Accounts");
 
-                xrmApp.CommandBar.ClickCommand("New");
+            xrmApp.CommandBar.ClickCommand("New");
 
-                var accountName = TestSettings.GetRandomString(5, 15);
-                xrmApp.Entity.SetValue("name", accountName);
-                xrmApp.Entity.Save();
+            var accountName = TestSettings.GetRandomString(5, 15);
+            xrmApp.Entity.SetValue("name", accountName);
+            xrmApp.Entity.Save();
 
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
 
-                xrmApp.Grid.Search(accountName);
+            xrmApp.Grid.Search(accountName);
 
-                xrmApp.ThinkTime(3000);
+            xrmApp.ThinkTime(3000);
 
-                xrmApp.Grid.OpenRecord(0);
+            xrmApp.Grid.OpenRecord(0);
 
-                xrmApp.CommandBar.ClickCommand("Delete");
+            xrmApp.CommandBar.ClickCommand("Delete");
 
-                xrmApp.Dialogs.ConfirmationDialog(true);
+            xrmApp.Dialogs.ConfirmationDialog(true);
 
-                xrmApp.ThinkTime(7000);
-
-            }
+            xrmApp.ThinkTime(7000);
         }
 
         [TestMethod]
         [TestCategory("TestersTalk")]
         public void EasyReproTest5()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
 
-                xrmApp.CommandBar.ClickCommand("New");
+            xrmApp.CommandBar.ClickCommand("New");
 
-                var accountName = TestSettings.GetRandomString(5, 15);
-                xrmApp.Entity.SetValue("name", accountName);
-                xrmApp.Entity.Save();
+            var accountName = TestSettings.GetRandomString(5, 15);
+            xrmApp.Entity.SetValue("name", accountName);
+            xrmApp.Entity.Save();
 
-                xrmApp.Entity.SelectTab("Related", "Contacts");
-                
-                xrmApp.RelatedGrid.ClickCommand("New Contact");
+            xrmApp.Entity.SelectTab("Related", "Contacts");
 
-                xrmApp.QuickCreate.SetValue("lastname", "TestersTalk");
+            xrmApp.RelatedGrid.ClickCommand("New Contact");
 
-                xrmApp.QuickCreate.Save();
+            xrmApp.QuickCreate.SetValue("lastname", "TestersTalk");
 
-                xrmApp.ThinkTime(4000);
+            xrmApp.QuickCreate.Save();
 
-            }
+            xrmApp.ThinkTime(4000);
         }
 
         [TestMethod]
@@ -172,20 +154,16 @@ namespace Microsoft.Dynamics365.UIAutomation.Sample.UCI
         DataAccessMethod.Sequential)]
         public void EasyReproDataDrivenTesting()
         {
-            var client = new WebClient(TestSettings.Options);
-            using (var xrmApp = new XrmApp(client))
-            {
-                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+            xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+            xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+            xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
 
-                xrmApp.CommandBar.ClickCommand("New");
+            xrmApp.CommandBar.ClickCommand("New");
 
-                xrmApp.Entity.SetValue("name", TestContext.DataRow["tag1"].ToString());
-                xrmApp.Entity.Save();
+            xrmApp.Entity.SetValue("name", testContextInstance.DataRow["tag1"].ToString());
+            xrmApp.Entity.Save();
 
-                xrmApp.ThinkTime(4000);
-            }
+            xrmApp.ThinkTime(4000);
         }
 
     }
---
+++ Microsoft.Dynamics365.UIAutomation.Sample/Util/TestBase.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Drawing;
+using System.Security;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample.Util
+{
+    [TestClass]
+    public class TestBase
+    {
+        /* static public SecureString _username;
+         static public SecureString _password;
+         static public SecureString _mfaSecretKey;
+
+         public static Uri _xrmUri;
+
+         public static string parentWindow;
+         public static WebClient client;
+         public static XrmApp xrmApp;*/
+
+        public static XrmPage xrmPage;
+        public static TestContext testContextInstance;
+        public static WebClient client;
+        public static XrmApp xrmApp;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+        /*  public static IWebDriver _driver;
+
+          public static TestContext testContextInstance;
+
+          public TestContext TestContext
+          {
+              get { return testContextInstance; }
+              set { testContextInstance = value; }
+          }
+
+          [AssemblyInitialize]
+          public static void AssemblyInitialize(TestContext context)
+          {
+              Console.WriteLine("Assembly Initilization...");
+              testContextInstance = context;
+              _username = testContextInstance.Properties["OnlineUsername"].ToString().ToSecureString();
+              _password = testContextInstance.Properties["OnlinePassword"].ToString().ToSecureString();
+              _mfaSecretKey = testContextInstance.Properties["MfaSecretKey"].ToString().ToSecureString();
+          }
+
+          [AssemblyCleanup]
+          public static void AssemblyCleanUp()
+          {
+              Console.WriteLine("Assembly Cleanup...");
+              _username.Clear();
+              _password.Clear();
+          }*/
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Console.WriteLine("Test started...");
+            /*   client = new WebClient(TestSettings.Options);
+               xrmApp = new XrmApp(client);
+               xrmPage = new XrmPage(client.Browser);
+               new Uri(testContextInstance.Properties["OnlineCrmUrl"].ToString());*/
+
+            client = new WebClient(TestSettings.Options);
+            xrmApp = new XrmApp(client);
+            xrmPage = new XrmPage(client.Browser);
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            Console.WriteLine("Test Cleanup...");
+
+            //Take screenshot when test failed
+            try
+            {
+                String status = testContextInstance.CurrentTestOutcome.ToString();
+                if (status.Equals("Failed") || status.Equals("Error"))
+                {
+                    xrmPage.Browser.Driver.Manage().Window.Size = new Size(1920, 1080);
+                }
+                var currentDataandTime = testContextInstance.TestName + "_" + DateTime.Now.ToString("MM_dd_yyyy_hh_mm_ss");
+                string fileName = string.Format("Failed - " + currentDataandTime + ".jpeg", testContextInstance.TestResultsDirectory);
+                xrmPage.Browser.TakeWindowScreenShot(fileName, ScreenshotImageFormat.Jpeg);
+                testContextInstance.AddResultFile(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            xrmPage.Browser.Driver.Quit();
+            xrmPage.Browser.ThinkTime(3000);
+        }
+    }
+}
